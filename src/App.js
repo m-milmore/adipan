4bb566aca3a86d0c444c942aab5484e496cc5bb3
Michel Milmore <m.milmore2701@gmail.com>
2022-08-12 19:49:55 -0400

    work on grid test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import "./App.css";
-
-import TodoApp from "./TodoApp";
-
-import MapFct from "./mapFct";
-
-
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function App() {
-  
-return (
+  return (
     <div className="App">
-      <header className="App-header">
-        <TodoApp />
-              </header>
+      <div className="container text-center">
+        <div>ADIPAN</div>
+        <Form>
+          <Row className="mb-3">
+            <Form.Group as={Col}>
+              <Form.Label>Numéro</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+
+            <Form.Group as={Col}>
+              <Form.Label>Date</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+
+            <Form.Group as={Col}>
+              <Form.Label>Mois</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+          </Row>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
